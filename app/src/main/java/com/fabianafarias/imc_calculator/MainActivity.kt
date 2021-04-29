ec38c9f4bb29e42ef67ab9d4ee7ef1e80bc1a6af
fabiana farias <farias.fabianam@gmail.com>
2021-04-29 19:05:52 -0300

    Creating the business logic of the calculator.

--- app/src/main/java/com/fabianafarias/imc_calculator/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/imc_calculator/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.fabianafarias.imc_calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import com.fabianafarias.imc_calculator.R.layout.activity_main
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+        setTextView()
     }
+
+    private fun setTextView() {
+        altura?.doAfterTextChanged { text ->
+
+        }
+
+        peso?.doOnTextChanged { text, start, before, count ->
+
+        }
+
+        calculate_button?.setOnClickListener {
+            calculateIMC(peso.text.toString(), altura.text.toString())
+        }
+    }
+
+    private fun calculateIMC(peso:String, altura:String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            titulo_resultado.text = "Seu IMC é:\n%.2f".format(imc)
+        }
+
+    }
+
+
+
 }
